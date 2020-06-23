510e97f2ba1e88583d1c96530361cb9dcb9bbb29
sheff3rd <thedoctorwhoovesmail@gmail.com>
2020-06-24 01:19:06 +0300

    use webgl 2.0

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var THREE = require('three');
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
+var canvas = document.createElement( 'canvas' );
+var context = canvas.getContext( 'webgl2', { alpha: false } );
+var renderer = new THREE.WebGLRenderer( { canvas: canvas, context: context } );
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement );
 
